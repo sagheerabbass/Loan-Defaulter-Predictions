486666602266ff82a293dfb0107763f05a80e85d
Sagheer Abbas <sagheerabbas298@gmail.com>
2025-11-09 10:46:16 +0500

    Made Project Loan Defaulter

---
+++ main.py
@@ -0,0 +1,28 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+from sklearn.ensemble import RandomForestClassifier 
+import pandas as pd 
+import numpy as np
+
+# Loading Data
+df=pd.read_csv('Loan.csv')
+
+# Seperate Features X,y
+X=df[['Income','Loan_Amount','Credit_Score']]
+y=df['Defaulted ']
+
+# Split Data into training and Testing
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+# Training Model
+RVC=RandomForestClassifier(n_estimators=100,random_state=42)
+RVC.fit(X_train,y_train)
+
+# Model Predictions
+y_pred=RVC.predict(X_test)
+
+#Model Evaluation
+print('Accuracy Score : \n',accuracy_score(y_test,y_pred))
+print('\n Classification Report: \n',classification_report(y_test,y_pred))
+print('\n Confusion Matrix: \n',confusion_matrix(y_test,y_pred))
+
